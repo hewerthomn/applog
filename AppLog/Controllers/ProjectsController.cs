a0f8cace0882162dcce0c537961683d710d4beac
Everton Inocencio <evertoninocencio@deti.ro.gov.br>
2014-12-10 10:36:28 -0400

    created form to create project

---
+++ AppLog/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppLog.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
