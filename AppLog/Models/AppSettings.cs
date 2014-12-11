53e2e2855adcfa3dfa52c5623ce3f31bb8a0ba25
Sylvio Tavares da Silva Júnio <sylvio.tavares@deti.ro.gov.br>
2014-12-11 13:10:17 -0400

    Added AppSettings

---
+++ AppLog/Models/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using AppLog.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppLog.Models
+{
+    public class AppSettings
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public object Get(string key, object defaultValue)
+        {
+            return AppSettingsRepository.Get(key, defaultValue);
+        }
+
+    }
+}
---
+++ AppLog/Models/Repository/AppSettingsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppLog.Models.Repository
+{
+    public class AppSettingsRepository : Repository
+    {
+        private static object GetKey(string key, object defaultValue)
+        {
+            AppSettings appSettings = db.AppSettings.FirstOrDefault(x => x.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase));
+            if (appSettings != null)
+            {
+                return appSettings.Value;
+            }
+
+            return defaultValue;
+        }
+
+        public static object Get(string key, object defaultValue)
+        {
+            return GetKey(key, defaultValue);
+        }
+
+        public static bool GetBoolean(string key, bool defaultValue)
+        {
+            object value = GetKey(key, defaultValue);
+            
+            return Convert.ToBoolean(value);
+        }
+
+        public static string GetString(string key, string defaultValue)
+        {
+            object value = GetKey(key, defaultValue);
+
+            return value.ToString();
+        }
+    }
+}
